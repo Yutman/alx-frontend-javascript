2769f27b1973762b7f3ca1edb719e74be1c61e73
Yutman <kuriapeter95@gmail.com>
2023-08-03 18:15:14 +0000

    Fix Readme

---
+++ 0x03-ES6_data_manipulation/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    // Your ESLint rules go here
+  },
+};
+
